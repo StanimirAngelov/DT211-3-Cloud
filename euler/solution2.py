7af9c3dcfd2fcc3bc5125f2f8c89f7ddfa7d9e0e
StanimirAngelov <C12733849@mydit.ie>
2014-10-02 14:44:24 +0000

    soln file

---
+++ euler/solution2.py
@@ -0,0 +1,20 @@
+def sequence(n):
+   if n <= 1:
+       return n
+   else:
+       return(sequence(n-1) + sequence(n-2))
+
+
+i = 1
+j = 0
+total = 0
+while j < 4000000:
+    j = sequence(i)
+    if j%2:
+        print(j)
+    else:
+        print(j)
+        total = total + j
+    i+=1
+print("total is")
+print(total)
